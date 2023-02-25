9e7aa1666c1b6e1b6a04c19e19ceab47a55c2dc1
syshen <sysheen@gmail.com>
2023-02-25 13:42:11 +0800

    fix bug

--- summarization.py
+++ summarization.py
@@ -22,6 +22,8 @@ def text_to_chunks(text, chunk_size=2000, overlap=100):
     if char in punctuation:
       sentences.append(text[start:i+1])
       start = i+1
+  if i < len(text):
+    sentences.append(text[start:])
 
   chunks = []
   chunk = None
@@ -40,23 +42,6 @@ def text_to_chunks(text, chunk_size=2000, overlap=100):
         chunk.extend(tokens)
       current_chunk_size += num_tokens
 
-
-  # paragraphs = []
-  # current_paragraph = ''
-  # for sentence in sentences:
-  #   current_paragraph += sentence.strip()
-  #   if sentence[-1] in '.!?':
-  #     paragraphs.append(current_paragraph)
-  #     current_paragraph = ''
-  
-  # tokens = tokenizer.encode(text)
-  # num_tokens = len(tokens)
-    
-  # chunks = []
-  # for i in range(0, num_tokens, chunk_size - overlap):
-  #   chunk = tokens[i:i + chunk_size]
-  #   chunks.append(chunk)
-    
   return chunks
 
 def callOpenAI(prompt_request, max_tokens=500):
@@ -78,7 +63,7 @@ def summarize_text(full_text):
 
   for i, chunk in enumerate(chunks):
     text = tokenizer.decode(chunks[i])
-    summary = callOpenAI(f"Summarize: {text}", max_tokens=1500)
+    summary = callOpenAI(f"Summarize: {text}", max_tokens=2000)
     summaries.append(summary)
 
   if len(summaries) > 0:
